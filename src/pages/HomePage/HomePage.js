9c0634a66c63d2160041a614639f6e20c40f6ea2
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-13 18:01:33 -0700

    Added filter by type functionality to home page.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,9 +7,38 @@ import List from "../../components/List";
 const HomePage = () => {
   // Set pokemonList state and declare it as an empty array
   const [pokemonList, setPokemonList] = useState([]);
-
   // Set search state and declare it as an empty string
   const [search, setSearch] = useState("");
+  // Set filteredPokemonList state and declare it as an empty array
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  // Initialize pokemonTypes and declare it as an array with the 15 types of pokemon
+  const pokemonTypes = [
+    "Bug",
+    "Dragon",
+    "Electric",
+    "Fighting",
+    "Fire",
+    "Flying",
+    "Ghost",
+    "Grass",
+    "Ground",
+    "Ice",
+    "Normal",
+    "Poison",
+    "Psychic",
+    "Rock",
+    "Water",
+  ];
+  // Set state for type checkboxes by using reduce on the pokemonTypes array to turn each type into an object with the type name for the key and false for the value
+  const [typeChecked, setTypeChecked] = useState(
+    pokemonTypes.reduce(
+      (types, acc) => ({
+        ...types,
+        [acc]: false,
+      }),
+      {}
+    )
+  );
 
   // Function to fetch list of pokemon & set the pokemonList state to the response data
   const fetchPokemon = async () => {
@@ -19,6 +48,7 @@ const HomePage = () => {
     setPokemonList(res.data.pokemon);
   };
 
+  // Function to display search results
   const searchPokemonHandler = (e) => {
     // Prevent default submission behavior (page reload)
     e.preventDefault();
@@ -36,11 +66,58 @@ const HomePage = () => {
     }
   };
 
+  // Function to reset search
   const searchResetHandler = () => {
     // Clear search field
     setSearch("");
     // Re-populate list of pokemon
     fetchPokemon();
+  };
+
+  // Function to filter pokemon
+  const filterPokemonTypeHandler = (e) => {
+    // Prevent default submission behavior (page reload)
+    e.preventDefault();
+
+    // Initialize a variable to store the types that are true from the typeChecked state
+    const typeFilters = [];
+    // Loop over the properties in the typeChecked state and add the types that evaluate to true to the typeFilters array
+    for (const type in typeChecked) {
+      if (typeChecked[type] === true) {
+        typeFilters.push(type);
+      }
+    }
+
+    // Check that filters exist
+    if (typeFilters.length) {
+      // Function to compare 2 arrays for the same content
+      const isSame = (arr1, arr2) =>
+        arr1.length === arr2.length &&
+        arr1.every((value, idx) => value === arr2[idx]);
+
+      // Filter the pokemonList to return any pokemon whose types meet the filter requirements
+      const filterResults = pokemonList.filter((pokemon) =>
+        isSame(pokemon.type.sort(), typeFilters.sort())
+      );
+
+      // Update list to show filter results if any exist
+      if (filterResults.length) {
+        setFilteredPokemonList(filterResults);
+      } else {
+        fetchPokemon();
+      }
+    } else {
+      setFilteredPokemonList([]);
+    }
+  };
+
+  // Function to handle type filter checkbox changes
+  const typeCheckboxHandler = (e) => {
+    // Set typeChecked state to maintain all of it's current values except where the key is equal to e.target.name, in which case it will toggle the value
+    setTypeChecked({
+      ...typeChecked,
+      [e.target.name]: !typeChecked[e.target.name],
+    });
   };
 
   // If the pokemonList state is an empty array, run the fetchPokemon function
@@ -71,7 +148,30 @@ const HomePage = () => {
           Reset
         </Button>
       </Form>
-      <List pokemonList={pokemonList} />
+      <h3>Filter Pokemon Type</h3>
+      <Form onSubmit={filterPokemonTypeHandler}>
+        <Form.Group controlId="formTypeFilterCheckbox">
+          <Form.Label>Type: </Form.Label>
+          {pokemonTypes.map((type, idx) => (
+            <Form.Check
+              key={idx}
+              type="checkbox"
+              label={type}
+              name={type}
+              checked={typeChecked[type]}
+              onChange={typeCheckboxHandler}
+            />
+          ))}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Apply Filter
+        </Button>
+      </Form>
+      <List
+        pokemonList={
+          filteredPokemonList.length ? filteredPokemonList : pokemonList
+        }
+      />
     </>
   );
 };
